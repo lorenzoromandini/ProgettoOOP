3c151e5d1681c7a6a889856b4a47ed9be1db5eb5
Lorenzo Romandini <lorenzoromandini@live.it>
2021-01-26 18:09:30 +0100

    Start

---
+++ TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/TicketmasterEuropeApplication.java
@@ -0,0 +1,13 @@
+package eu.univpm.TicketmasterEurope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketmasterEuropeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketmasterEuropeApplication.class, args);
+	}
+
+}
---
+++ TicketmasterEurope/src/test/java/eu/univpm/TicketmasterEurope/TicketmasterEuropeApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.univpm.TicketmasterEurope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketmasterEuropeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
