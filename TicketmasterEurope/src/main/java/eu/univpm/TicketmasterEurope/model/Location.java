f3508080839a7d5e29a551e172c94216e5f09d0c
Lorenzo Romandini <lorenzoromandini@live.it>
2021-01-28 11:52:52 +0100

    Update Location

--- TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/model/Location.java
+++ TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/model/Location.java
@@ -3,6 +3,9 @@
  */
 package eu.univpm.TicketmasterEurope.model;
 
+import java.util.Vector;
+
+
 /**
  * @author loren
  *
@@ -14,6 +17,7 @@ public class Location {
 	private String city;
 	private String country;
 	private String countrycode;
+	private Vector<Event> evento = new Vector<Event>();
 	
 	
 	public Location() {
@@ -46,11 +50,11 @@ public class Location {
 	 * @param city
 	 * @param country
 	 */
-	public Location(String address, String city, String country) {
+	public Location(String address, String city, String countrycode) {
 		super();
 		this.address = address;
 		this.city = city;
-		this.country = country;
+		this.countrycode = countrycode;
 	}
 
 
@@ -149,55 +153,85 @@ public class Location {
 	public void setCountrycode(String countrycode) {
 		this.countrycode = countrycode;
 	}
+	
+	
+	/**
+     * Metodo che restituisce il vettore di event.
+     * @return vector.
+     */
+	public Vector<Event> getVector() {
+		return evento;
+	}
 
+	/**
+     * Metodo che setta il vettore di event.
+     * @param vettore di event.
+     */
+	public void setVector(Vector<Event> evento) {
+		this.evento = evento;
+	}
+	
+	/**
+     * Metodo che scrive il vettore come una stringa.
+     * @return String toReturn che rappresenta le informazioni essenziali relative all'evento.
+     */
+	public String toStringVector() {
+		String toReturn = "";
+		for (int i = 0; i < evento.size(); i++)
+			toReturn += evento.get(i).toString();
+		return toReturn;
+	}
 
 	/**
 	 * Override del metodo toString.
-	 * @return String che rappresenta la città.
+	 * @return String che rappresenta la località e la sua ubicazione.
 	 */
 	@Override
-	public String toString() {
-		return "placement = " + placement + ", address = " + address + ", city = " + city +
-				", country =" + country + ", countrycode = " + countrycode;
-		
+	public String toString() {		
+		return "placement = " + placement + ", address = " + address + ", city = " + city + ", country =" + country +
+				", countrycode = " + countrycode + ", eventArray = " + toStringVector();
 	}
 	
 	
+	/**
+	 * Override del metodo equals.
+	 * @param oggetto Location da confrontare.
+	 * @return true o false a seconda che i due oggetti siano uguali.
+	 */
 	@Override
-	public boolean equals(Object obj) {
+	public boolean equals(Object obj) {		
 		if (this == obj)
 			return true;
 		if (obj == null)
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Location loc = (Location) obj;
+		Location other = (Location) obj;
 		if (placement == null) {
-			if (loc.placement != null)
+			if (other.placement != null)
 				return false;
-		} else if (!placement.equals(loc.placement))
+		} else if (!placement.equals(other.placement))
 			return false;
 		if (address == null) {
-			if (loc.address != null)
+			if (other.address != null)
 				return false;
-	} else if (!address.equals(loc.address))
+	} else if (!address.equals(other.address))
 			return false;
 		if (city == null) {
-			if (loc.city != null)
+			if (other.city != null)
 				return false;
-		} else if (!city.equals(loc.city))
+		} else if (!city.equals(other.city))
 			return false;
 		if (country == null) {
-			if (loc.country != null)
+			if (other.country != null)
 				return false;
-	   } else if (!country.equals(loc.country))
+	   } else if (!country.equals(other.country))
 			return false;
 		if (countrycode == null) {
-			if (loc.countrycode != null)
+			if (other.countrycode != null)
 				return false;
-	   } else if (!countrycode.equals(loc.countrycode))
+	   } else if (!countrycode.equals(other.countrycode))
 			return false;
-		
 		return true;
 	}
 	
