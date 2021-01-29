343e37889aa56f8829754dcec1e71b414033bab6
Lorenzo Romandini <lorenzoromandini@live.it>
2021-01-29 12:49:00 +0100

    Creation Controller

---
+++ TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/controller/Controller.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package eu.univpm.TicketmasterEurope.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author loren
+ *
+ */
+
+@RestController
+public class Controller {
+	
+	
+
+	
+
+}
