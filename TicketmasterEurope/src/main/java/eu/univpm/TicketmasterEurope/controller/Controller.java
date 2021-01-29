568538ac23d94460f8a2fde73b393c5e20a4bc90
Lorenzo Romandini <lorenzoromandini@live.it>
2021-01-29 17:46:43 +0100

    Implements a @GetMapping method getCountryEvents

--- TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/controller/Controller.java
+++ TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/controller/Controller.java
@@ -3,15 +3,49 @@
  */
 package eu.univpm.TicketmasterEurope.controller;
 
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import eu.univpm.TicketmasterEurope.model.*;
+
+import eu.univpm.TicketmasterEurope.service.Service;
+
 
 /**
- * @author loren
- *
+ * @author Lorenzo Romandini
+ * @author Nicholas Urbanelli
  */
 
 @RestController
+
 public class Controller {
+	
+	@Autowired
+	Service service;
+	
+	
+	
+	@GetMapping(value = "/events") 
+	public ResponseEntity<Object> getCountryEvents(@RequestParam String country) {
+		
+		Location location = service.getCountryEventsfromApi(country);
+		
+		JSONObject obj = new JSONObject();
+		ToJSON tojson = new ToJSON();
+		
+		obj = tojson.toJson(country);
+		
+		return new ResponseEntity<> (obj.toString(), HttpStatus.OK);
+    }
+		
+	
+	
+		
+}
 	
 	
 
