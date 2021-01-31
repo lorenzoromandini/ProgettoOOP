a140f24b37dde872e16bed0e2b8aeec2e07c88a2
Lorenzo Romandini <lorenzoromandini@live.it>
2021-01-31 19:28:21 +0100

    Completed the method getCountryEventsSelectedfromApi

--- TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/service/ServiceManagement.java
+++ TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/service/ServiceManagement.java
@@ -9,13 +9,10 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.web.client.RestTemplate;
 
-import com.project.WeatherApp.model.City;
-import com.project.WeatherApp.model.Coordinates;
-import com.project.WeatherApp.model.Weather;
+import eu.univpm.TicketmasterEurope.model.*;
 
-import eu.univpm.TicketmasterEurope.model.Location;
-
-/**
+/** Questa classe è l'implementazione dell'interfaccia Service
+ * 
  * @author Lorenzo Romandini
  * @author Nicholas Urbanelli
  */
@@ -33,55 +30,82 @@ public class ServiceManagement implements Service {
 	 */
 	public JSONObject getCountryEvents(String countryCode) {
 		
-		JSONObject CountryEventsObject;
-		String url = "https://app.ticketmaster.com/discovery/v2/events.json?&countryCode=" + countryCode + "&apikey="+ apikey;
+		JSONObject countryEventsObject;
+		String Url = "https://app.ticketmaster.com/discovery/v2/events.json?&countryCode=" + countryCode + "&apikey="+ apikey;
 		
 		RestTemplate restTemplate = new RestTemplate();
 		
-		CountryEventsObject = new JSONObject(restTemplate.getForObject(url, String.class));
+		countryEventsObject = new JSONObject(restTemplate.getForObject(Url, String.class));
 		
-		return CountryEventsObject;
+		return countryEventsObject;
 		
 	}
 
+	/**
+	 * Questo metodo utilizza getCountryEvents per andare a selezionare le informazioni desiderate relative ad un evento
+	 * (id, nome, url, info, data, orario, valuta, prezzo minimo, prezzo massimo, tipologia, genere e sottogenere)
+	 * @param countryCode - codice del paese in cui ha luogo l'evento
+	 * @return Location location - oggetto contenente le informazioni desiderate dell'evento e il luogo in cui si svolge
+	 */
 	public Location getCountryEventsSelectedfromApi(String countryCode) {
 		
-		JSONObject CountryEventsSelectedObject = getCountryEvents(countryCode);
+		JSONObject countryEventsSelectedObject = getCountryEvents(countryCode);
 		
 		Location location = new Location(countryCode);
 		
 		location = getCountryfromApi(countryCode);
 		
-		JSONArray countryArray = CountryEventsSelectedObject.getJSONArray("events");  
-		JSONObject ;
+		JSONArray countryEventsArray = countryEventsSelectedObject.getJSONArray("events");  
+		JSONObject object;
 		
-		Vector<Weather> vector = new Vector<Weather>(weatherArray.length());
-		
-		
-	
+		Vector<Event> vector = new Vector<Event>(countryEventsArray.length());
 			
+		for (int i = 0; i < countryEventsArray.length(); i++) {
 			
-			for (int i = 0; i<weatherArray.length(); i++) {
-				
-				Weather weather = new Weather();
-				counter = weatherArray.getJSONObject(i); // assegna a counter il JSONObject corrispondente all'i-esimo JSONObject sottoinsieme di list
-				weather.setVisibility(counter.getInt("visibility")); // visibility è stand-alone
-				weather.setData(counter.getString("dt_txt"));
-				JSONArray arrayW = counter.getJSONArray("weather"); // i-esimo sottoinsieme weather di list
-				JSONObject objectW = arrayW.getJSONObject(0); 
-				weather.setDescription(objectW.getString("description")); // campo description del sottoinsieme weather
-				weather.setMain(objectW.getString("main")); // campo main del sottoinsieme weather
-				JSONObject objectW2 = counter.getJSONObject("main");  // i-esimo sottoinsieme main di list
-				weather.setTemp_max(objectW2.getDouble("temp_max")); // campo temp_max del sottoinsieme main
-				weather.setTemp_min(objectW2.getDouble("temp_min"));
-				weather.setFeels_like(objectW2.getDouble("feels_like"));
-				vector.add(weather); 
-		
+			Event event = new Event();
+			
+			object = countryEventsArray.getJSONObject(i);
+			event.setName(object.getString("name"));
+			event.setId(object.getString("id"));
+			event.setUrl(object.getString("url"));
+			event.setInfo(object.getString("info"));
+			
+			Date data = new Date();
+			JSONArray datesArray = object.getJSONArray("dates");
+			JSONObject datesObject = datesArray.getJSONObject(0);
+			JSONArray startDateArray = datesObject.getJSONArray("start");
+			JSONObject startDateObject = startDateArray.getJSONObject(0);
+			data.setData(startDateObject.getString("localDate"));
+			data.setOrario(startDateObject.getString("localTime"));
+			event.setDate(data);
+			
+			Prices prices = new Prices();
+			JSONArray pricesArray = object.getJSONArray("priceRanges");
+			JSONObject pricesObject = pricesArray.getJSONObject(0);
+			prices.setCurrency(pricesObject.getString("currency"));
+			prices.setMaxPrice(pricesObject.getDouble("max"));
+			prices.setMinPrice(pricesObject.getDouble("min"));
+			event.setFeatures(prices);
+			
+			Genre genre = new Genre();
+			JSONArray classificationsArray = object.getJSONArray("classifications");
+			JSONObject classificationsObject = classificationsArray.getJSONObject(0);
+			JSONArray segmentArray = classificationsObject.getJSONArray("segment");
+			JSONObject segmentObject = segmentArray.getJSONObject(0);
+			genre.setSegmentName(segmentObject.getString("name"));
+			JSONArray genreArray = classificationsObject.getJSONArray("genre");
+			JSONObject genreObject = genreArray.getJSONObject(0);
+			genre.setGenreName(genreObject.getString("name"));
+			JSONArray subGenreArray = classificationsObject.getJSONArray("subGenre");
+			JSONObject subGenreObject = subGenreArray.getJSONObject(0);
+			genre.setSubGenreName(subGenreObject.getString("name"));	
+			event.setGenre(genre);
+
 			}
+						
+		location.setEvento(vector);
 		
-		city.setVector(vector);
-		
-		return city;
+		return location;
 		
 	}
 	
@@ -93,22 +117,9 @@ public class ServiceManagement implements Service {
 		Location location = new Location(countryCode);
 		
 			
-			JSONObject cityObj = object.getJSONObject("city");
-			String country = (String) cityObj.get("country");
-			int id = (int) cityObj.get("id");
-			JSONObject coordinatesObj = cityObj.getJSONObject("coord");
-			double latitude = (double) coordinatesObj.get("lat");
-			double longitude = (double) coordinatesObj.get("lon");
-			Coordinates coordinates = new Coordinates(latitude,longitude); 
-			city.setCountry(country);
-			city.setId(id);
-			city.setCoordinates(coordinates);
-		
-		return city;
+			
 	}
 	
-
-	public 
 
 	public JSONObject getMarketEvents(int marketId) {
 		
