34c0bd3e1210073df032a47f28994803cc70b905
Lorenzo Romandini <lorenzoromandini@live.it>
2021-02-05 22:24:39 +0100

    Updating Date class

--- TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/model/Date.java
+++ TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/model/Date.java
@@ -12,63 +12,99 @@ package eu.univpm.TicketmasterEurope.model;
 public class Date {
 	
 	/**
-	 *  Indica la data (di inizio e fine) dell'evento
+	 *  Indica la data di inizio dell'evento
 	 */
-	private String data;
+	private String dataInizio;
 	
 	/** 
-	 * Indica l'orario (di inizio e fine) dell'evento
+	 * Indica l'orario di inizio dell'evento
 	 */
-	private String orario;
+	private String orarioInizio;
+	
+	/**
+	 *  Indica la data di terminazione dell'evento
+	 */
+	private String dataFine;
+	
+	/** 
+	 * Indica l'orario di terminazione dell'evento
+	 */
+	private String orarioFine;
 	
 	
 	/** Costruttore dell'oggetto
 	 */
 	public Date() {
-		this.data = null;
-		this.orario = null;	}
-
-
-	/** Costruttore dell'oggetto
-	 * @param data - data (di inizio e fine) dell'evento
-	 * @param orario - orario (di inizio e fine) dell'evento
-	 */
-	public Date(String data, String orario) {
-		this.data = data;
-		this.orario = orario;
-	}
+		this.dataInizio = null;
+		this.orarioInizio = null;	
+		this.dataFine = null;
+		this.orarioFine = null;
+		}
 	
 	
-	/** Restituisce la data dell'evento
-	 * @return data
+	/** Restituisce la data di inizio dell'evento
+	 * @return dataInizio
 	 */
-	public String getData() {
-		return data;
+	public String getDataInizio() {
+		return dataInizio;
 	}
-
-
-	/** Setta la data dell'evento
-	 * @param String data
-	 */
-	public void setData(String data) {
-		this.data = data;
-	}
-
  
-	/** Restituisce l'orario dell'eventp
-	 * @return orario
+
+	/** Setta la data di inizio dell'evento
+	 * @param String dataInizio
 	 */
-	public String getOrario() {
-		return orario;
+	public void setDataInizio(String dataInizio) {
+		this.dataInizio = dataInizio;
 	}
 
 
-	/** Setta l'orario dell'evento
-	 * @param String orario
+	/** Restituisce l'orario di inizio dell'evento
+	 * @return orarioInizio
 	 */
-	public void setOrario(String orario) {
-		this.orario = orario;
+	public String getOrarioInizio() {
+		return orarioInizio;
 	}
+
+
+	/** Setta l'orario di terminazione dell'evento
+	 * @param String orarioInizio
+	 */
+	public void setOrarioInizio(String orarioInizio) {
+		this.orarioInizio = orarioInizio;
+	}
+
+
+	/** Restituisce la data di terminazione dell'evento
+	 * @return dataFine
+	 */
+	public String getDataFine() {
+		return dataFine;
+	}
+
+
+	/** Setta la data di terminazione dell'evento
+	 * @param String dataFine
+	 */
+	public void setDataFine(String dataFine) {
+		this.dataFine = dataFine;
+	}
+
+
+	/** Restituisce l'orario di terminazione dell'evento
+	 * @return orarioFine
+	 */
+	public String getOrarioFine() {
+		return orarioFine;
+	}
+
+
+	/** Setta l'orario di terminazione dell'evento
+	 * @param String orarioFine
+	 */
+	public void setOrarioFine(String orarioFine) {
+		this.orarioFine = orarioFine;
+	}
+
 	
 	/**
 	 * Override del metodo toString.
@@ -76,7 +112,8 @@ public class Date {
 	 */
 	@Override
 	public String toString() {
-		return "data = " + data + ", orario = " + orario + "";
+		return "dataInizio=" + dataInizio + ", orarioInizio=" + orarioInizio + ", dataFine=" + dataFine
+				+ ", orarioFine=" + orarioFine + "";
 	}
 
 
@@ -86,7 +123,7 @@ public class Date {
 	 * @return true o false a seconda che i due oggetti siano uguali.
 	 */
 	@Override
-	public boolean equals(Object obj) {		
+	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
@@ -94,27 +131,28 @@ public class Date {
 		if (getClass() != obj.getClass())
 			return false;
 		Date other = (Date) obj;
-		if (data == null) {
-			if (other.data != null)
+		if (dataFine == null) {
+			if (other.dataFine != null)
 				return false;
-		} else if (!data.equals(other.data))
+		} else if (!dataFine.equals(other.dataFine))
 			return false;
-		if (orario == null) {
-			if (other.orario != null)
+		if (dataInizio == null) {
+			if (other.dataInizio != null)
 				return false;
-		} else if (!orario.equals(other.orario))
+		} else if (!dataInizio.equals(other.dataInizio))
 			return false;
-		return true;		
+		if (orarioFine == null) {
+			if (other.orarioFine != null)
+				return false;
+		} else if (!orarioFine.equals(other.orarioFine))
+			return false;
+		if (orarioInizio == null) {
+			if (other.orarioInizio != null)
+				return false;
+		} else if (!orarioInizio.equals(other.orarioInizio))
+			return false;
+		return true;
 	}
-
-
-	
-	
-	
-	
-	
-
-	
 	
 
 }
