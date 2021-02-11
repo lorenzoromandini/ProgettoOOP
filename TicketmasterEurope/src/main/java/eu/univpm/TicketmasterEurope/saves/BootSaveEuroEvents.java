1ff4d6d26a75587f9670fbb5244a7a5cf2f22a13
Lorenzo Romandini <lorenzoromandini@live.it>
2021-02-11 15:43:34 +0100

    Class that implements che booting method of saving events in european countries

---
+++ TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/saves/BootSaveEuroEvents.java
@@ -0,0 +1,1237 @@
+/**
+ * 
+ */
+package eu.univpm.TicketmasterEurope.saves;
+
+
+public class BootSaveEuroEvents {
+	
+	public void SaveBootEvents() {
+
+		
+		SaveRelevantEvents save = new SaveRelevantEvents();
+		
+		try {
+			
+			save.StoreRelevantEvents("AT", "Arts & Theatre");
+			
+		}  catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			
+			save.StoreRelevantEvents("AT", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			
+			save.StoreRelevantEvents("AT", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+
+			save.StoreRelevantEvents("AT", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("BE", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("BE", "Miscellaneous");
+			
+		}  catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("BE", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("BE", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("BG", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("BG", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("BG", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("BG", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("HR", "Arts & Theatre");
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("HR", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("HR", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("HR", "Sports");
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+
+			save.StoreRelevantEvents("CY", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("CY", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("CY", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("CY", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("CZ", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("CZ", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("CZ", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("CZ", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			
+			save.StoreRelevantEvents("DK", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("DK", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("DK", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("DK", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("FO", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("FO", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("FO", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("FO", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("FI", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("FI", "Miscellaneous");
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("FI", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("FI", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("FR", "Arts & Theatre");
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("FR", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			
+			save.StoreRelevantEvents("FR", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("FR", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			
+			save.StoreRelevantEvents("GE", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GE", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GE", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GE", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("DE", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("DE", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("DE", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("DE", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GI", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GI", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GI", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GI", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GB", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GB", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GB", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GB", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GR", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GR", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GR", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("GR", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("HU", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("HU", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("HU", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("HU", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			
+			save.StoreRelevantEvents("IS", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("IS", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("IS", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("IS", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("IE", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("IE", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("IE", "Music");
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("IE", "Sports");	
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("IT", "Arts & Theatre");
+				
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("IT", "Miscellaneous");
+
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("IT", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("IT", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("LT", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("LT", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("LT", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+		
+			save.StoreRelevantEvents("LT", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("LU", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("LU", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("LU", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("LU", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("MT", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("MT", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("MT", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("MT", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("MC", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("MC", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("MC", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("MC", "Sports");
+
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("ME", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("ME", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("ME", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("ME", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("NL", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("NL", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("NL", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("NL", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("ND", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("ND", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("ND", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("ND", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("NO", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("NO", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("NO", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("NO", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("PL", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("PL", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("PL", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("PL", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		
+		try {
+			
+			save.StoreRelevantEvents("PT", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("PT", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("PT", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("PT", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("RO", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("RO", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("RO", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("RO", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			
+			save.StoreRelevantEvents("RU", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("RU", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("RU", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("RU", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("RS", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("RS", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("RS", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("RS", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("SK", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("SK", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("SK", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("SK", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("SI", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("SI", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("SI", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("SI", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("ES", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("ES", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("ES", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("ES", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("SE", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			
+			save.StoreRelevantEvents("SE", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("SE", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("SE", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("CH", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("CH", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("CH", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("CH", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("UA", "Arts & Theatre");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("UA", "Miscellaneous");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("UA", "Music");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		try {
+			
+			save.StoreRelevantEvents("UA", "Sports");
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+						
+	}
+	
+	
+}
+	
