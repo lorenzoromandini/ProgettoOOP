d0df26adf124540ba9c0db7ebd598b20167905be
Lorenzo Romandini <lorenzoromandini@live.it>
2021-02-11 15:46:39 +0100

    Adding the boot save method in main class

--- TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/TicketmasterEuropeApplication.java
+++ TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/TicketmasterEuropeApplication.java
@@ -1,13 +1,24 @@
 package eu.univpm.TicketmasterEurope;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import eu.univpm.TicketmasterEurope.saves.BootSaveEuroEvents;
+
 
 @SpringBootApplication
 public class TicketmasterEuropeApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(TicketmasterEuropeApplication.class, args);
+		
+		
+		BootSaveEuroEvents boot = new BootSaveEuroEvents();
+		
+		boot.SaveBootEvents();
+		
 	}
-
+	
 }
