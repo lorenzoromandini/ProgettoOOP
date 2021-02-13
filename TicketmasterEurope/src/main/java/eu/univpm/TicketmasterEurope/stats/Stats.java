6920247d2e4d1559d70097b595b801bd179cf433
Lorenzo Romandini <lorenzoromandini@live.it>
2021-02-13 01:37:59 +0100

    Thrown exceptions and added comments

--- TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/stats/Stats.java
+++ TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/stats/Stats.java
@@ -5,17 +5,23 @@ package eu.univpm.TicketmasterEurope.stats;
 
 import org.json.JSONObject;
 
-/**
+import eu.univpm.TicketmasterEurope.exception.WrongCountryException;
+import eu.univpm.TicketmasterEurope.exception.WrongValueException;
+
+
+/** Questa classe è l'interfaccia di StatsManagement
+ * 
  * @author Lorenzo Romandini
  * @author Nicholas Urbanelli
+ * 
  */
 public interface Stats {
 	
-	public abstract JSONObject getCountryEvents(String countryCode, int period);
-	public abstract JSONObject getMarketEvents(String marketId, int period);
-	public abstract JSONObject getSegmentEvents(String countryCode, String segment, int period);
-	public abstract JSONObject getGenreEvents(String countryCode, String genre, int period);
-	public abstract JSONObject getSubGenreEvents(String countryCode, String subGenre, int period);
-	public abstract JSONObject getSourceEvents(String countryCode, String source, int period);
+	public abstract JSONObject getCountryEvents(String countryCode, int period) throws WrongCountryException;
+	public abstract JSONObject getMarketEvents(String marketId, int period) throws WrongCountryException, WrongValueException;
+	public abstract JSONObject getSegmentEvents(String countryCode, String segment, int period) throws WrongValueException, WrongCountryException;
+	public abstract JSONObject getGenreEvents(String countryCode, String genre, int period) throws WrongCountryException, WrongValueException;
+	public abstract JSONObject getSubGenreEvents(String countryCode, String subGenre, int period) throws WrongCountryException, WrongValueException;
+	public abstract JSONObject getSourceEvents(String countryCode, String source, int period) throws WrongCountryException, WrongValueException;
 
 }
