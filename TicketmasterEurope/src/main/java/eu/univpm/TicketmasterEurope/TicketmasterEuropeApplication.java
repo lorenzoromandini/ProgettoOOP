9e5dc330099c765ed9cb3bfc09503a6ec4dbdcf6
Lorenzo Romandini <lorenzoromandini@live.it>
2021-02-13 01:37:17 +0100

    Added comments

--- TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/TicketmasterEuropeApplication.java
+++ TicketmasterEurope/src/main/java/eu/univpm/TicketmasterEurope/TicketmasterEuropeApplication.java
@@ -8,13 +8,24 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import eu.univpm.TicketmasterEurope.saves.BootSaveEuroEvents;
 
 
+/** Avvia l'applicazione Spring
+ * 
+ * @author Lorenzo Romandini
+ * @author Nicholas Urbanelli
+ * 
+ */
+
 @SpringBootApplication
+
 public class TicketmasterEuropeApplication {
 
 	public static void main(String[] args) throws IOException {
+		
 		SpringApplication.run(TicketmasterEuropeApplication.class, args);
 		
-		
+		/** Una volta avviato il programma, salverà su una serie di file gli eventi più rilevanti per tipologia relativi
+		 * a tutti i paesi europei, richiamando il metodo SaveBootEvents della classe BootSaveEuroEvents
+		 */		
 		BootSaveEuroEvents boot = new BootSaveEuroEvents();
 		
 		boot.SaveBootEvents();
